3a77fd66430f5b8a997de875f6375a5496c0cd44
Alex Leifermann <aleif92@outlook.com>
2025-02-08 12:36:11 -0600

    First Commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,41 @@
+﻿using LearningToCodeMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace LearningToCodeMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            
+            return View(dogs);
+        }
+
+        
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index)); 
+
+           // return View("Index");
+
+        }
+
+        public string Hello()
+        {
+            return "whos there?";
+        }
+    }
+}
